f0edacc69427f0ea4aca5f7135d7dbc756bf54cc
ViktorK66 <55109336+ViktorK66@users.noreply.github.com>
2019-10-14 01:08:48 +0300

    Add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  const lengthConfig = bracketsConfig.length;
+  for (let i = 0; i < str.length; i++) {
+    for (let f = 0; f < lengthConfig; f++) {
+      if (bracketsConfig[f].includes(str[i])) {
+        if (str[i] == bracketsConfig[f][1] && brackets[brackets.length - 1] == bracketsConfig[f][0]) {
+          brackets.pop();
+        } else {
+          brackets.push(str[i]);
+        }
+      }
+    }
+  }
+  if (!brackets.length) {
+    return true;
+  }
+  return false;
 }
